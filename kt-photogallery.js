ad2f3292b2c790501ea6e6453290b30a4d0f46ec
kungtiger <kungtiger@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-03 11:50:59 +0000

    1.1 release
    git-svn-id: https://plugins.svn.wordpress.org/kt-photogallery/tags/1.1@1104059 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ kt-photogallery.js
@@ -0,0 +1,195 @@
+(function ($, l10n, ajaxurl, type) {
+    $(function () {
+        // a bit hacky. prevent the grid metabox from beeing sortable
+        $('#exposed-sortables').removeClass('meta-box-sortables');
+
+        var $document = $(document);
+        var $Wrap = $('#griddiv');
+        var $Add = $('#add');
+        var $Grid = $(type == 'photogallery' ? '#album_grid' : '#image_grid');
+        var cssAjax = 'ui-ajax';
+
+        // thumbnail chooser
+        if (type == 'photoalbum') {
+            var $Choose_Thumbnail = $('#choose_thumbnail');
+            var $Clear_Thumbnail = $('#clear_thumbnail');
+            var $Thumbnail_ID = $('#thumbnail_id');
+            var $Thumbnail_Figure = $('#thumbnail_preview');
+            var $Thumbnail_IMG = $Thumbnail_Figure.children('img');
+
+            // create a new thumbnail if none is set
+            if ($Thumbnail_IMG.length == 0) {
+                $Thumbnail_IMG = $('<img alt />');
+            }
+
+            // init a WordPress Media Dialog for choosing a thumbnail
+            var wp_media_Thumbnail = wp.media({
+                title: l10n.thumbnail,
+                multiple: false,
+                library: {
+                    type: 'image'
+                },
+                button: {
+                    text: l10n.use
+                }
+            });
+
+            // set selected image as thumbnail
+            wp_media_Thumbnail.on('select', function () {
+                wp_media_Thumbnail.state().get('selection').each(function (attachment) {
+                    var sizes = attachment.attributes.sizes;
+                    $Thumbnail_ID.val(attachment.id);
+                    $Thumbnail_IMG.attr({
+                        title: attachment.attributes.title,
+                        src: sizes.thumbnail ? sizes.thumbnail.url : sizes.full.url
+                    }).appendTo($Thumbnail_Figure);
+                });
+                wp_media_Thumbnail.close();
+            });
+
+            // choose a thumbnail
+            $Choose_Thumbnail.on('click', function (e) {
+                wp_media_Thumbnail.open();
+            });
+
+            // clear chosen thumbnail
+            $Clear_Thumbnail.on('click', function () {
+                $Thumbnail_IMG.detach();
+                $Thumbnail_ID.val('');
+            });
+        }
+
+        // add dialog
+        if (type == 'photogallery') {
+            var $Dialog = $('#album_dialog');
+            var $DialogButton;
+            var timestamp = Date.now() - 6e4;
+            var buttons = { };
+            var _load_albums_nonce = $('#_load_albums_nonce').val();
+
+            // add selected albums to the grid
+            buttons[l10n.add] = function () {
+                var $selection = selectsort_Dialog.selection();
+                selectsort_Dialog.deselect();
+                $selection.clone().appendTo($Grid);
+                $Dialog.dialog("close");
+            };
+
+            // init a jQuery UI Dialog for album selection
+            $Dialog.dialog({
+                autoOpen: false,
+                buttons: buttons,
+                closeText: l10n.close,
+                height: 600,
+                minHeight: 400,
+                minWidth: 563,
+                modal: true,
+                title: l10n.title,
+                width: 800,
+                open: function () {
+                    // reload albums every 60 seconds
+                    if (timestamp < (Date.now() - 6e4)) {
+                        $Dialog.addClass(cssAjax).empty();
+                        $.ajax({
+                            url: ajaxurl,
+                            type: 'POST',
+                            dataType: 'text',
+                            data: {
+                                _load_albums_nonce: _load_albums_nonce,
+                                action: 'load_albums'
+                            },
+                            success: function (albums) {
+                                if (albums && albums != '-1') {
+                                    $Dialog.html(albums);
+                                    timestamp = Date.now();
+                                }
+                            },
+                            complete: function () {
+                                $Dialog.removeClass(cssAjax);
+                            }
+                        });
+                    }
+                },
+                create: function () {
+                    // main dialog button needs to be a primary button and disabled by default
+                    $DialogButton = $(this).next().find('button').attr('disabled', true).addClass('button-primary');
+                }
+            });
+        } else {
+            // init a WordPress Media Dialog for image selection
+            var wp_media_Images = wp.media({
+                title: l10n.title,
+                multiple: true,
+                library: {
+                    type: 'image'
+                },
+                button: {
+                    text: l10n.add
+                }
+            });
+
+            // add selected images to the grid
+            wp_media_Images.on('select', function () {
+                var url;
+                var images = [ ];
+                wp_media_Images.state().get('selection').each(function (attachment) {
+                    var sizes = attachment.attributes.sizes;
+                    url = sizes.thumbnail ? sizes.thumbnail.url : sizes.full.url;
+                    images.push('<figure class="image" title="' + attachment.attributes.title + '"><input type="hidden" name="images[]" value="' + attachment.id + '"><span class="kt-thumbnail"><img src="' + url + '" /></span></figure>');
+                });
+                images.reverse();
+                $Grid.append(images.join(''));
+                wp_media_Images.close();
+            });
+        }
+
+        // open a dialog to select items to be added to the grid
+        $Add.on('click', function () {
+            if (type == 'photogallery') {
+                $Dialog.dialog('open');
+            } else {
+                wp_media_Images.open();
+            }
+        });
+
+        // remove button
+        var $Remove = $('#remove');
+        $Remove.on('click', function () {
+            selectsort_Grid.selection().remove();
+            $Wrap.removeClass('removeable');
+        });
+
+        // catch delete and backspace keys
+        var maybeDelete = function (e) {
+            if (e.which == 8 || e.which == 46) {
+                $Remove.trigger('click');
+                $document.off('keydown', maybeDelete);
+                e.preventDefault();
+            }
+        };
+
+        // init SelectSort for jQuery UI Dialog
+        if (type == 'photogallery') {
+            var selectsort_Dialog = SelectSort($Dialog, {
+                sortable: false,
+                filter: 'figure'
+            });
+            selectsort_Dialog.on('change', function ($selection) {
+                $DialogButton.attr('disabled', $selection.length == 0);
+            });
+        }
+
+        // init SelectSort for grid
+        var selectsort_Grid = SelectSort($Grid, {
+            filter: 'figure',
+            helper: function ($selection) {
+                var $stack = $selection.slice(0, 3).find('img').clone();
+                return $('<div class="ui-sort-helper" />').append($stack);
+            }
+        });
+        selectsort_Grid.on('change', function ($selection) {
+            $Wrap.toggleClass('removeable', $selection.length > 0);
+            $document[$selection.length ? 'on' : 'off']('keydown', maybeDelete);
+        });
+    });
+})(jQuery, kt_Photogallery_l10n, ajaxurl, typenow);
---
+++ selectsort-1.2.js
@@ -0,0 +1,431 @@
+/*
+ * SelectSort 1.2
+ * by Daniel Schneider - https://profiles.wordpress.org/kungtiger
+ *
+ * Released under GPLv2 or later
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+window.SelectSort = (function ($) {
+    var limit = function (min, x, max) {
+        return Math.max(min, Math.min(x, max));
+    };
+    var ctrl = function (e) {
+        return e.ctrlKey || e.shiftKey || e.metaKey;
+    };
+    var eventNames = function (string) {
+        return String(string).replace(/^\s+|\s+$/, '').split(/\s+/);
+    };
+    var Instances = [ ];
+    var SelectSort = function (target, init) {
+        var O = SelectSort.get(target);
+        if (O !== null) {
+            return O;
+        }
+        var $grid = $(target).first();
+        if ($grid.length == 0) {
+            return null;
+        }
+        var grid = $grid[0];
+        var Events = { };
+        O = $.extend({
+            grid: grid,
+            select: function (target) {
+                select(the(target));
+                return this;
+            },
+            deselect: function (target) {
+                deselect(the(target));
+                return this;
+            },
+            selection: function () {
+                return selection();
+            },
+            cancel: function () {
+                cancel();
+                return this;
+            },
+            selecting: function () {
+                return selecting;
+            },
+            sorting: function () {
+                return sorting;
+            },
+            on: function (name, fn) {
+                if (typeof fn == 'function') {
+                    $.each(eventNames(name), function () {
+                        if (!Events[this]) {
+                            Events[this] = [ ];
+                        }
+                        Events[this].push(fn);
+                    });
+                }
+                return this;
+            },
+            off: function (name, fn) {
+                var empty = typeof fn != 'function';
+                $.each(eventNames(name), function () {
+                    var name = this;
+                    if (empty) {
+                        Events[name] = [ ];
+                    } else {
+                        $.each(Events[name], function (i) {
+                            if (this == fn) {
+                                Events[name].splice(i, 1);
+                                return false;
+                            }
+                        });
+                    }
+                });
+                return this;
+            },
+            trigger: function (name) {
+                var parameters = Array.prototype.slice.call(arguments, 1);
+                $.each(eventNames(name), function () {
+                    if (Events[this]) {
+                        $.each(Events[this], function () {
+                            this.apply(O, parameters);
+                        });
+                    }
+                });
+                return this;
+            },
+            destroy: function () {
+                $document.off('mousemove', prepare);
+                $document.off('mousemove', marquee);
+                $document.off('mousemove', follow);
+                $document.off('mouseup', stop);
+                $document.off('keydown', cancel);
+                $grid.off('mousedown', start);
+                $grid.off('mousedown', deselect);
+                $grid.off('mousemove', O.filter, sort);
+                $grid.off('mouseup', select);
+                $grid.removeClass(SelectSort.selecting);
+                $grid.removeClass(SelectSort.sorting);
+                children().removeClass(SelectSort.selected).removeClass(SelectSort.sorting);
+                $marquee.remove();
+                if ($helper) {
+                    $helper.remove();
+                }
+                for (var i = 0, l = Instances.length; i < l; i++) {
+                    if (Instances[i] == O) {
+                        Instances.splice(i, 1);
+                    }
+                }
+                delete O;
+            }
+        }, SelectSort.fn, init);
+        var $document = $(document);
+        var $current, $selection, $helper;
+        var $marquee = $('<div class="' + SelectSort.marquee + '"/>');
+        var a, offsetX, offsetY, x0, y0, width, height, Cache;
+        var deselectable = true;
+        var beyond = false;
+        var selecting = false;
+        var sorting = false;
+        var children = function () {
+            return $grid.children(O.filter);
+        };
+        var selection = function () {
+            return children().filter('.' + SelectSort.selected);
+        };
+        var the = function (mixed) {
+            if (mixed) {
+                var $elements;
+                if (typeof mixed == 'string') {
+                    $elements = children().filter(mixed);
+                } else {
+                    $elements = $(mixed);
+                }
+                return $elements.filter(function () {
+                    return this.parentElement == grid;
+                });
+            }
+            return $();
+        };
+        var select = function (e) {
+            if (e.jquery) {
+                if (e.length) {
+                    if (O.multiple) {
+                        e.addClass(SelectSort.selected);
+                    } else {
+                        children().removeClass(SelectSort.selected);
+                        e.first().addClass(SelectSort.selected);
+                    }
+                } else {
+                    return;
+                }
+            } else if (!O.multiple) {
+                children().removeClass(SelectSort.selected);
+                $current.addClass(SelectSort.selected);
+            } else if (e.ctrlKey || e.metaKey) {
+                $current.toggleClass(SelectSort.selected);
+            } else {
+                var $children = children();
+                $children.removeClass(SelectSort.selected);
+                if (e.shiftKey && null !== a) {
+                    var b = $current.index();
+                    $children.slice(Math.min(a, b), Math.max(a, b) + 1).addClass(SelectSort.selected);
+                } else {
+                    $current.addClass(SelectSort.selected);
+                }
+            }
+            dispatch();
+        };
+        var deselect = function (e) {
+            var all = false;
+            if (e.jquery) {
+                all = e.length == 0;
+                if (!all) {
+                    e.removeClass(SelectSort.selected);
+                    dispatch();
+                }
+            } else {
+                all = (e.target == grid || $(e.target).closest(grid).length == 0) && !ctrl(e);
+            }
+            if (all) {
+                children().removeClass(SelectSort.selected);
+                $grid.off('mousedown', deselect);
+                deselectable = true;
+                O.trigger('deselect');
+                O.trigger('change', $());
+                a = null;
+            }
+        };
+        var dispatch = function () {
+            var $selection = selection();
+            var any = $selection.length > 0;
+            if (any && deselectable) {
+                $grid.on('mousedown', deselect);
+                deselectable = false;
+            } else if (!deselectable && !any) {
+                $grid.off('mousedown', deselect);
+                deselectable = true;
+            }
+            if (any) {
+                O.trigger('select', $selection);
+            } else {
+                O.trigger('deselect');
+            }
+            any ? O.trigger('select', $selection) : O.trigger('deselect');
+            O.trigger('change', $selection);
+        };
+        var start = function (e) {
+            var l = children().length;
+            if (l && O.selectable && e.which == 1) {
+                x0 = e.pageX;
+                y0 = e.pageY;
+                if (e.target == grid) {
+                    if (O.multiple) {
+                        selecting = true;
+                        $document.on('mousemove', prepare);
+                    }
+                } else {
+                    $current = $(e.target).closest(O.filter);
+                    if (!e.shiftKey) {
+                        a = $current.index();
+                    }
+                    if ($current.hasClass(SelectSort.selected)) {
+                        $grid.one('mouseup', select);
+                    } else {
+                        select(e);
+                    }
+                    if (O.sortable) {
+                        $selection = selection();
+                        if ($selection.length < l) {
+                            sorting = true;
+                            $document.on('mousemove', prepare);
+                        }
+                    }
+                }
+            }
+        };
+        var prepare = function (e) {
+            e.preventDefault();
+            var dx = x0 - e.pageX;
+            var dy = y0 - e.pageY;
+            beyond = O.distance <= 0 || Math.sqrt(dx * dx + dy * dy) > O.distance;
+            if (beyond) {
+                var offset = $grid.offset();
+                offsetX = offset.left;
+                offsetY = offset.top;
+                selecting ? prepare_select() : prepare_sort();
+                $document.off('mousemove', prepare);
+                $document.on('keydown', cancel);
+            }
+        };
+        var prepare_select = function () {
+            x0 -= offsetX;
+            y0 -= offsetY;
+            var paddingX = parseInt($grid.css('paddingLeft'));
+            var paddingY = parseInt($grid.css('paddingTop'));
+            Cache = children().map(function () {
+                var $this = $(this);
+                var position = $this.position();
+                return {
+                    $: $this,
+                    x: position.left + paddingX,
+                    y: position.top + paddingY,
+                    w: $this.outerWidth(),
+                    h: $this.outerHeight(),
+                    s: $this.hasClass(SelectSort.selected)
+                };
+            }).get();
+            width = $grid.innerWidth();
+            height = $grid.innerHeight();
+            $marquee.css({
+                width: '0px',
+                height: '0px'
+            });
+            $grid.append($marquee);
+            $grid.addClass(SelectSort.selecting);
+            $document.on('mousemove', marquee);
+            O.trigger('start');
+        };
+        var prepare_sort = function () {
+            Cache = children().map(function () {
+                var $this = $(this);
+                return {
+                    $: $this,
+                    x: $this.offset().left,
+                    w: $this.outerWidth() / 2
+                };
+            }).get();
+            $grid.addClass(SelectSort.sorting);
+            $grid.off('mouseup', select);
+            $grid.on('mousemove', O.filter, sort);
+            if (O.helper) {
+                if (typeof O.helper == 'function') {
+                    $helper = $(O.helper.call(O, $selection));
+                } else {
+                    $helper = $(O.helper);
+                }
+                $helper.css({
+                    left: x0,
+                    top: y0
+                });
+                $grid.append($helper);
+                $document.on('mousemove', follow);
+            }
+            O.trigger('start', $selection);
+        };
+        var marquee = function (e) {
+            e.preventDefault();
+            var x1 = limit(0, e.pageX - offsetX, width);
+            var y1 = limit(0, e.pageY - offsetY, height);
+            var w = Math.abs(x0 - x1);
+            var h = Math.abs(y0 - y1);
+            var x = Math.min(x0, x1);
+            var y = Math.min(y0, y1);
+            $marquee.css({
+                left: x,
+                top: y,
+                width: w + 'px',
+                height: h + 'px'
+            });
+            $.each(Cache, function (i, c) {
+                c.$.toggleClass(SelectSort.selected, c.s);
+                if (!(c.x + c.w < x || x + w < c.x || c.y + c.h < y || y + h < c.y)) {
+                    c.$.toggleClass(SelectSort.selected, !(ctrl(e) && c.s));
+                }
+            });
+            var $selection = selection();
+            O.trigger('change', $selection);
+        };
+        var sort = function (e) {
+            e.preventDefault();
+            var $this = $(this);
+            if (!$this.hasClass(SelectSort.selected)) {
+                var i = $this.index();
+                $this[(e.pageX - Cache[i].x < Cache[i].w) ? 'before' : 'after']($selection);
+                O.trigger('sorting', $selection);
+            }
+        };
+        var follow = function (e) {
+            $helper.css({
+                left: e.pageX - offsetX,
+                top: e.pageY - offsetY
+            });
+        };
+        var cancel = function (e) {
+            if (!e || e.which == 27) {
+                if (selecting) {
+                    $.each(Cache, function () {
+                        this.$.toggleClass(SelectSort.selected, this.s);
+                    });
+                } else {
+                    $.each(Cache, function () {
+                        $grid.append(this.$);
+                    });
+                }
+                stop();
+                O.trigger('cancel');
+                if (e) {
+                    e.preventDefault();
+                }
+            }
+        };
+        var stop = function (e) {
+            if (beyond) {
+                $document.off('keydown', cancel);
+                if (selecting) {
+                    $grid.removeClass(SelectSort.selecting);
+                    $marquee.detach();
+                    $document.off('mousemove', marquee);
+                    if (e) {
+                        dispatch();
+                        O.trigger('stop');
+                    }
+                } else {
+                    $grid.removeClass(SelectSort.sorting);
+                    $grid.off('mousemove', O.filter, sort);
+                    if (O.helper) {
+                        $helper.remove();
+                        $document.off('mousemove', follow);
+                    }
+                    if (e) {
+                        $.each(Cache, function (i) {
+                            if (i !== this.$.index()) {
+                                O.trigger('update');
+                                return false;
+                            }
+                        });
+                        O.trigger('stop');
+                    }
+                }
+                beyond = false;
+            } else {
+                $document.off('mousemove', prepare);
+            }
+            selecting = false;
+            sorting = false;
+        };
+        $grid.on('mousedown', start);
+        $document.on('mouseup', stop);
+        Instances.push(O);
+        return O;
+    };
+    $.extend(SelectSort, {
+        selected: 'ui-selected',
+        selecting: 'ui-selecting',
+        sorting: 'ui-sorting',
+        marquee: 'ui-marquee',
+        fn: {
+            selectable: true,
+            multiple: true,
+            sortable: true,
+            distance: 7,
+            filter: 'li'
+        },
+        get: function (target) {
+            var grid = $(target)[0];
+            for (var i = 0, l = Instances.length; i < l; i++) {
+                if (Instances[i].grid == grid) {
+                    return Instances[i];
+                }
+            }
+            return null;
+        }
+    });
+    return SelectSort;
+})(jQuery);
